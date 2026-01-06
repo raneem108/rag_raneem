ab87a7ac2b6738d4c2dfbd16c911c41d483d8b9c
raneem <rabujabal4@gmail.com>
2026-01-06 18:04:10 +0300

    pp

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
-from routes import base 
 app = FastAPI()
-app.include_router(base.base_router)
+
+@app.get("/welcome")
+def welcome():
+    return{
+        "message" : "Hello world!"
+    }
